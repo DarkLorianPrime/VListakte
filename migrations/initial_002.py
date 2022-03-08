d629077a8de156a8b72d102587cbf25d8f2a23de
DarkLorian <66025673+DarkLorianPrime@users.noreply.github.com>
2022-03-09 02:50:12 +0400

    Add files via upload

--- migrations/initial_002.py
+++ migrations/initial_002.py
@@ -7,10 +7,13 @@ from libraries.database import insert_migration, delete_migration
 
 
 async def nextmigration(db: Database):
+    await db.execute(
+        """CREATE TABLE Account(id SERIAL PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), password VARCHAR(255), email VARCHAR(255))""")
     await insert_migration(db, os.path.basename(__file__).split(".")[0], datetime.now())
 
 
 async def rollbackmigration(db: Database):
     timestamp = datetime.now()
     name = os.path.basename(__file__)
+    await db.execute("""DROP TABLE Account""")
     await delete_migration(db, name, timestamp)
--- migrations/initial_003.py
+++ migrations/initial_003.py
@@ -9,10 +9,14 @@ from libraries.database import insert_migration, delete_migration
 async def nextmigration(db: Database):
     timestamp = datetime.now()
     name = os.path.basename(__file__).split(".")[0]
+    await db.execute("""ALTER TABLE Account ADD Phone CHARACTER VARYING(20) NOT NULL DEFAULT '7 (000) 000 00-00'""")
+    await db.execute("""ALTER TABLE Account ADD Phone_mom CHARACTER VARYING(20) NOT NULL DEFAULT '7 (000) 000 00-00'""")
     await insert_migration(db, name, timestamp)
 
 
 async def rollbackmigration(db: Database):
     timestamp = datetime.now()
     name = os.path.basename(__file__)
+    await db.execute("""ALTER TABLE Account DROP Phone""")
+    await db.execute("""ALTER TABLE Account DROP Phone_mom""")
     await delete_migration(db, name, timestamp)
