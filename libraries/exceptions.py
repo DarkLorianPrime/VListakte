f4dc73cf1dc5dc1f3db152827f7ad3f54ffe8f40
darklorianprime <lolpokens@mail.ru>
2022-06-22 21:06:53 +0400

    fix requirements

--- libraries/exceptions.py
+++
@@ -1,2 +0,0 @@
-class MigratorError(Exception):
-    pass
--- libraries/files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def check_file(name, folder):
-    return list(filter(lambda file: file.startswith(name), os.listdir(folder)))
-
-
-def check_file_end(name, folder):
-    return list(filter(lambda file: file.endswith(name), os.listdir(folder)))
